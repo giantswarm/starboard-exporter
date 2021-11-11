e79113fbedd80211a60951e84e25b1852c5d97dc
Zach Stone <zach@giantswarm.io>
2021-11-11 18:06:03 +0100

    Add existing API

--- main.go
+++ main.go
@@ -30,6 +30,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	"github.com/giantswarm/starboard-exporter/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -74,6 +76,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.VulnerabilityReportReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "VulnerabilityReport")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
