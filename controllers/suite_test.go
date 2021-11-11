b06893411df4a3bb90b8d0fdc2870ff502032f8c
Zach Stone <zach@giantswarm.io>
2021-11-11 18:10:51 +0100

    Ignore deadcode check in generated test

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -35,7 +35,7 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
+var cfg *rest.Config // nolint:deadcode
 var k8sClient client.Client
 var testEnv *envtest.Environment
 
