5bb95f595c9a333c7b8c8cc6becd1b691f65dccd
Zach Stone <zach@giantswarm.io>
2021-11-16 12:58:12 +0100

    Import and reconcile VulnerabilityReport

--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module github.com/giantswarm/starboard-exporter
 go 1.16
 
 require (
-	k8s.io/apimachinery v0.22.1
-	k8s.io/client-go v0.22.1
-	sigs.k8s.io/controller-runtime v0.10.0
+	github.com/aquasecurity/starboard v0.13.0
+	k8s.io/apimachinery v0.22.3
+	k8s.io/client-go v0.22.3
+	sigs.k8s.io/controller-runtime v0.10.2
 )
 
 replace (
--- main.go
+++ main.go
@@ -78,6 +78,7 @@ func main() {
 
 	if err = (&controllers.VulnerabilityReportReconciler{
 		Client: mgr.GetClient(),
+		// Log:    ctrl.Log.WithName("controllers").WithName("VulnerabilityReport"),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "VulnerabilityReport")
