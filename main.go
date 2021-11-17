8921c94eca3065c164c9cd5c514eaea87fc942c5
Zach Stone <zach@giantswarm.io>
2021-11-17 09:59:38 -0800

    Wip reconcile (#7)
    * Logging
    
    * Tidy
    
    * Use correct health probe port, add network policy for metrics
    
    * Add networkpolicy template name
    
    * Make circle a bit faster for testing
    
    * Some reconcile logic
    
    * tidy
    
    * Print report info
    
    * Add a metric
    
    * Try something
    
    * Handle nil scores
    
    * Set Service port for metrics
    
    * Change port in CM
    
    * Add summary metric and fix service targetport
    
    * Log cleanup
    
    * Register the other metric
    
    * Cleanup

--- main.go
+++ main.go
@@ -86,7 +86,7 @@ func main() {
 
 	if err = (&controllers.VulnerabilityReportReconciler{
 		Client: mgr.GetClient(),
-		// Log:    ctrl.Log.WithName("controllers").WithName("VulnerabilityReport"),
+		Log:    ctrl.Log.WithName("controllers").WithName("VulnerabilityReport"),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "VulnerabilityReport")
