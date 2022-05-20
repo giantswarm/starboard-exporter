6c3aba87362b37452ab0b469b90bc55c52dd322f
Zach Stone <zach@giantswarm.io>
2022-05-20 15:57:41 -0400

    Bump resource class for build job (OOMing)

--- controllers/key.go
+++ controllers/key.go
@@ -3,8 +3,8 @@ package controllers
 import "time"
 
 const (
-	ShardOwnerLabel    = "starboard-exporter.giantswarm.io/shard-owner"
 	DefaultServiceName = "starboard-exporter"
+	ShardOwnerLabel    = "starboard-exporter.giantswarm.io/shard-owner"
 )
 
 var DefaultRequeueDuration = (time.Minute * 5)
